df74cd42a50629dbfbc2a766b003e45dad1cdb0b
oluwagbogo <oluwagbogoadebanjo@gmail.com>
2024-11-08 01:56:14 +0100

    feat:navbar sorted

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
+import React from 'react';
+import Navbar from '../components/Navbar/Navbar';
+import HeroSection from '@/Home/HeroSection';
+
 export default function Home() {
   return (
     <div className="justify-center ">
-      Welcome to KalytoMed!
+      <Navbar />
+      <HeroSection/>
     </div>
   );
 }
