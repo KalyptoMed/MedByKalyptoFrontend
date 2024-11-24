a0dc6be50e442c21afc866e555067cfd4c275e95
oluwagbogo <oluwagbogoadebanjo@gmail.com>
2024-11-25 00:36:23 +0100

    feat:Explore screen implemented

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import HeroSection from '@/Home/HeroSection';
 import Carousel from '@/Home/Carousel';
-
+import Explore from '@/Home/Explore';
 export default function Home() {
   return (
     <div className="justify-center ">
       <HeroSection/>
       <Carousel/>
+      <Explore/>
     </div>
   );
 }
--- src/components/Home/Carousel.tsx
+++ src/components/Home/Carousel.tsx
@@ -5,51 +5,99 @@ import Image from "next/image";
 import { useCallback, useEffect, useState } from "react";
 
 const slideData = [
-  { src: "/assets/images/afrobabe.png", alt: "Bone and Joints", label: "Bone and Joints" },
-  { src: "/assets/images/drugicons.png", alt: "Eyedrops", label: "Eyedrops" },
-  { src: "/assets/images/drugicons.png", alt: "Pain Relief", label: "Pain Relief" },
-  { src: "/assets/images/drugicons.png", alt: "Vitamins", label: "Vitamins" },
-  { src: "/assets/images/drugicons.png", alt: "Skin Care", label: "Skin" },
-  { src: "/assets/images/drugicons.png", alt: "Skin Care", label: "Skin Care 1" },
-  { src: "/assets/images/drugicons.png", alt: "Skin Care", label: "Skin Care 2" },
-  { src: "/assets/images/drugicons.png", alt: "Skin Care", label: "Skin Care 3" },
+  {
+    src: "/assets/images/afrobabe.png",
+    alt: "Bone and Joints Pains",
+    label: "Bone and Joints",
+    bgColor: "bg-[#012ce6]",
+  },
+  {
+    src: "/assets/images/eyedrop.png",
+    alt: "Eyedrops",
+    label: "Eyedrops",
+    bgColor: "bg-[#854084]",
+  },
+  {
+    src: "/assets/images/close-up-hands-with-medical-devices.png",
+    alt: "Pain Relief",
+    label: "Pain Relief",
+    bgColor: "bg-[#6fa087]",
+  },
+  {
+    src: "/assets/images/ophthalmologist.png",
+    alt: "Eye care",
+    label: "Eye care",
+    bgColor: "bg-[#012ce6]",
+  },
+  {
+    src: "/assets/images/inhaler.png",
+    alt: "Respiratory",
+    label: "Respiratory",
+    bgColor: "bg-[#854084]",
+  },
+  {
+    src: "/assets/images/chest.png",
+    alt: "Kidney",
+    label: "Kidney",
+    bgColor: "bg-[#6fa087]",
+  },
+  {
+    src: "/assets/images/close-up-hands-with-medical-devices.png",
+    alt: "Medical Devices",
+    label: "Medical Devices",
+    bgColor: "bg-[#012ce6]",
+  },
+  {
+    src: "/assets/images/chest.png",
+    alt: "Ulcer",
+    label: "Ulcer",
+    bgColor: "bg-[#854084]",
+  },
 ];
 
 const Carousel = () => {
   const [emblaRef, emblaApi] = useEmblaCarousel({ loop: true, align: "start" });
-  const [selectedIndex, setSelectedIndex] = useState(0);
-
-  const onSelect = useCallback(() => {
+  
+  const autoplay = useCallback(() => {
     if (!emblaApi) return;
-    setSelectedIndex(emblaApi.selectedScrollSnap());
+    const timer = setInterval(() => {
+      emblaApi.scrollNext(); // Move to the next slide
+    }, 2000); // Adjust the delay in milliseconds
+
+    return () => clearInterval(timer); // Cleanup the interval on unmount
   }, [emblaApi]);
 
   useEffect(() => {
-    if (!emblaApi) return;
-    emblaApi.on("select", onSelect);
-  }, [emblaApi, onSelect]);
+    autoplay(); // Start autoplay when emblaApi is ready
+  }, [autoplay]);
 
   return (
-    <div className="w-full py-4 bg-[#F2F3F4]">
+    <div className="w-full py-6  pl-1 md:pl-4 bg-[#F2F3F4]">
       <div className="embla" ref={emblaRef}>
         <div className="embla__container flex">
           {slideData.map((slide, index) => (
-            <div key={index} className="embla__slide min-w-[38%] md:min-w-[20%] lg:min-w-[15%] p-1">
-              <div className="flex flex-col items-center bg-slate-400 rounded-2xl h-full">
+            <div
+              key={index}
+              className="embla__slide min-w-[38%] md:min-w-[20%] lg:min-w-[13%] p-1 relative"
+            >
+              <div
+                className={`flex flex-col items-center  rounded-2xl h-full ${slide.bgColor}`}
+              >
                 <Image
                   src={slide.src}
                   alt={slide.alt}
                   width={100}
                   height={100}
-                  className="w-full h-24 rounded-md object-contain"
+                  className="w-full h-32 rounded-md object-contain"
                 />
-                <p className="mb-2 text-sm font-medium text-gray-700">{slide.label}</p>
+                <p className="mb-2 text-sm font-medium text-white absolute bottom-1">
+                  {slide.label}
+                </p>
               </div>
             </div>
           ))}
         </div>
       </div>
-    
     </div>
   );
 };
