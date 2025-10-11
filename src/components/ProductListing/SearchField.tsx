f72bdbb9c574a124418e8e768e91bc78cf5fae70
Oluwagbogo <oluwagbogoadebanjo@gmail.com>
2025-10-11 14:46:31 +0100

    feat:product screen

--- src/components/ProductListing/SearchField.tsx
+++ src/components/ProductListing/SearchField.tsx
@@ -3,23 +3,16 @@ import Image from "next/image";
 function SearchField() {
   return (
     <Fragment>
-      <div className="flex justify-between ml-12 mr-96 px-32 mt-8 py-2 bg-white border rounded-full">
-        <div className="flex items-center w-full mr-1">
-          <Image
-            src="/assets/Images/Icon (1).png"
-            alt="Icon"
-            width={20}
-            height={20}
-            className="mr-1"
-          />
+      <div className="flex justify-between mt-40 py-2 px-10">
+        <div className="flex items-center w-3/4 mr-2 ">
           <input
             type="text"
             placeholder="Antibiotics"
-            className="flex rounded-full py-1 px-1"
+            className="flex rounded-full p-3 w-full"
           />
         </div>
 
-        <button className="mr-1 p-2 rounded-full bg-white border border-100 absolute right-20 transform translate-x-0">
+        <button className="p-4 rounded-full bg-white border border-100 absolute right-20 transform translate-x-0">
           {/* onClick={handleClick}> */}
           <Image
             src="/assets/Images/sliders-horiz-2.png"
